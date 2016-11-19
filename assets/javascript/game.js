3f4caa021c5c4982bb7e3cfbf2b16b27b093e5ae
Michael Emmanuel <michael.emmanuel732@gmail.com>
2016-11-18 21:11:56 -0500

    P game

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var psyLetter = ;
+var userLetter = "";
+var letterPressed = ;
+var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
