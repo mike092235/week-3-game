bea656ad103289ff601d7dfbedf81984188f0c05
Michael Emmanuel <michael.emmanuel732@gmail.com>
2016-11-18 20:15:44 -0500

    Psychic Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
-words = {};
-words.list = [];
+// words = {};
+// words.list = [];
 
-words.list[0] = "love";
-words.list[1] = "cat";
-words.list[2] = "steam";
+// words.list[0] = "love";
+// words.list[1] = "cat";
+// words.list[2] = "steam";
 
 
-words.length = words.list.length();
+// words.length = words.list.length();
+
+if(2 > 1){
+  alert("Two is greater than 1");
+};
